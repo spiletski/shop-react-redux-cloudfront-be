39d142e7174927dd9b58a95154daad954c94fce8
spiletski <stanislau@mail.ru>
2023-04-27 18:17:12 +0200

    task-7: authorization

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -20,6 +20,13 @@ export default {
             'Content-Type': 'application/json',
           },
         },
+        authorizer: {
+          name: 'BasicAuthorizer',
+          arn: 'arn:aws:lambda:us-east-1:883718311022:function:authorization-service-dev-basicAuthorizer',
+          resultTtlInSeconds: 0,
+          identitySource: "method.request.header.Authorization",
+          type: 'token',
+        }
       },
     },
   ],
